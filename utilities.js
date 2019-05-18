02472c84d930df60882f5caf2bacaa2b5d55ab77
ezin666 <ezin12@163.com>
2019-05-18 17:18:01 -0300

    Move more utility functions

--- utilities.js
+++ utilities.js
@@ -41,6 +41,74 @@ function clearDraw() {
 }
 
 /* For poll */
+function showPollResult(result) {
+  result.sort(function(l, r) {
+    return r.votes - l.votes;
+  });
+
+  var output = "<table> \
+    <caption>投票统计结果</caption> \
+    <tr>\
+      <th>选项</th>\
+      <th>票数</th>\
+      <th>投票者</th>\
+    </tr>";
+
+  result.forEach(function(value) {
+    output += "<tr>\
+        <td>" + (value.index + 1) + "</td>\
+        <td>" + value.votes + "</td> \
+        <td>" + value.commenters.join("<br/>") + "</td>\
+      </tr>"
+  });
+
+  document.getElementById("pollresult").innerHTML = output;
+}
+
+function addPollOption() {
+  var polloption = document.createElement('fieldset');
+  polloption.className = "polloption";
+  
+  var optionInput = document.createElement('input');
+  optionInput.type = "text";
+  optionInput.placeholder = "关键词 (选填)";
+  optionInput.name = "keyword";
+
+  polloption.innerHTML = "<div class='optionremove' onclick='removeOption(this)'>x</div>" + 
+    "<label>" + (document.getElementById("polloptions").children.length + 1) + "</label>";
+  polloption.appendChild(optionInput);
+  document.getElementById("polloptions").appendChild(polloption);
+
+  enableDisablePollButton(false);
+}
+
+function removeOption(el) {
+  var polloptions = document.getElementById("polloptions");
+  polloptions.removeChild(el.parentElement);
+
+  renumberPollOptions();
+}
+
+function renumberPollOptions() {
+  var polloptions = document.getElementById("polloptions").children;
+  var l = polloptions.length;
+  var optionlabel;
+  for (var i = 0; i < l; ++i) {
+    optionlabel = polloptions[i].getElementsByTagName("label")[0];
+    optionlabel.innerHTML = (i + 1);
+  }
+
+  enableDisablePollButton(l <= 0);
+}
+
+function enableDisablePollButton(b) {
+  document.getElementById("pollbutton").disabled = b;
+}
+
+function isMultiSelect() {
+  return document.getElementById("multiselect").checked;
+}
+
 function markKeyInString(str, keyArray) {
   return str.substring(0, keyArray[0]) + "<mark>" + str.substr(keyArray[0], keyArray[1]) + "</mark>" + str.substring(keyArray[0] + keyArray[1], str.length);
 }
